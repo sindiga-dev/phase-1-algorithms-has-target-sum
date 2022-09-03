42b72eaf9536100de2ebc3a27540f65f6cd267b8
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-09-03 13:15:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
+// function hasTargetSum(array, target) {
+// Write your algorithm here
+
+// solution one
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+	for (let i = 0; i < array.length; i++) {
+		const complement = target - array[i];
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] === complement) return true;
+		}
+	}
+	return false;
+}
+// solution 2
+function hasTargetSum(array, target) {
+	// create an object to keep track of all the numbers we've seen
+	const seenNumbers = {};
+	// iterate over the array of numbers
+	for (const number of array) {
+		// for the current number, identify a complementary number that adds to our target
+		// (for example: if our number is 2, and the target is 5, the complementary number is 3)
+		const complement = target - number;
+		// check if any of the keys in our object is the complement to the current number
+		// if so, return true
+		if (seenNumbers[complement]) return true;
+		// save the current number as the key on our object so we can check it later against other numbers
+		seenNumbers[number] = true;
+	}
+	// if we reach the end of the array, return false
+	return false;
+}
+
+// solution 3
+function hasTargetSum(array, target) {
+	const seenNumbers = new Set(); // initialize an empty Set
+	for (const number of array) {
+		const complement = target - number;
+
+		// .has returns true if the Set includes the complement
+		if (seenNumbers.has(complement)) return true;
+
+		// .add adds the number to the Set
+		seenNumbers.add(number);
+	}
+	return false;
 }
 
 /* 
@@ -16,19 +59,19 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+	console.log("Expecting: true");
+	console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+	console.log("Expecting: false");
+	console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
